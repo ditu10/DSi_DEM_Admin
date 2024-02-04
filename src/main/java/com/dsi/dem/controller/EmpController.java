270cb4c0a8efe9ba93360860e26d7248e9673a53
Ditu <oyasiul.islam_ditu@yahoo.com>
2024-02-05 01:24:06 +0600

    emp details

--- src/main/java/com/dsi/dem/controller/EmpController.java
+++ src/main/java/com/dsi/dem/controller/EmpController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Controller
@@ -38,7 +40,17 @@ public class EmpController {
     @GetMapping("/employee/{id}")
     public String showEmployee(@PathVariable("id") int id, Model model){
         Employee e = empService.getEmpById(id);
+        LocalDate today = LocalDate.now();
+        LocalDate t = e.getJoiningDate();
+
+        long year = ChronoUnit.YEARS.between(t, today);
+        long month = ChronoUnit.MONTHS.between(t,today);
+
+        month = month%12;
+
         model.addAttribute("emp",e);
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
         return "employee";
     }
 }
